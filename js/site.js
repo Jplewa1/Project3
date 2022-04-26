fc8840432d1545de6b7d282dd08edcec6f89db75
Jplewa1 <Jplewa1@hawk.iit.edu>
2022-04-25 23:09:08 -0500

    change marker location

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 function initMap() {
-  const chicago = {lat: 41.85, lng: -87.65};
+  const chicago = {lat: 41.83480515321117, lng: -87.62700590000001};
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 16,
     center: chicago,
