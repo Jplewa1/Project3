79649cd7218a6e7f48363ad62a1753a6f65c260e
Jplewa1 <Jplewa1@hawk.iit.edu>
2022-04-25 22:50:54 -0500

    fix marker

--- js/site.js
+++ js/site.js
@@ -1,14 +1,3 @@
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: 41.834683, lng: -87.625958},
-    zoom: 16,
-  });
-}
-
-window.initMap = initMap;
-
 function initMap() {
     const chicago = {lat: 41.85, lng: -87.65};
     const map = new google.maps.Map(document.getElementById("map"), {
