11ac4d24097a0251115bda36b04303ab73e0e1f5
Jplewa1 <Jplewa1@hawk.iit.edu>
2022-04-25 23:08:17 -0500

    fix cascade

--- js/site.js
+++ js/site.js
@@ -1,9 +1,9 @@
 function initMap() {
-    const chicago = {lat: 41.85, lng: -87.65};
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
-      center: chicago,
-      mapTypeId: 'terrain'
+  const chicago = {lat: 41.85, lng: -87.65};
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: chicago,
+    mapTypeId: 'terrain'
     });
     const marker = new google.maps.Marker({
       position: chicago,
