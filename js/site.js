f4a0de6b8a5bad9cb97741ae8ed1bb5c569fc7c2
Jplewa1 <Jplewa1@hawk.iit.edu>
2022-04-25 23:17:18 -0500

    icon pic

--- js/site.js
+++ js/site.js
@@ -5,6 +5,10 @@ function initMap() {
     center: chicago,
     mapTypeId: 'terrain'
     });
+    var icon = {
+        url: "https://upload.wikimedia.org/wikipedia/commons/a/a1/Ambigram_Home_Away_-_black_vector.svg",
+        scaledSize: new google.maps.Size(100, 100),
+    };
     const marker = new google.maps.Marker({
       position: chicago,
       map: map,
