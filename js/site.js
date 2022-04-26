676007c2e58178cc8c158a45be8aecb088076a53
Jplewa1 <Jplewa1@hawk.iit.edu>
2022-04-25 22:43:11 -0500

    try add marker

--- js/site.js
+++ js/site.js
@@ -8,3 +8,17 @@ function initMap() {
 }
 
 window.initMap = initMap;
+
+function initMap() {
+    const chicago = {lat: 41.85, lng: -87.65};
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: chicago,
+    });
+    const marker = new google.maps.Marker({
+      position: chicago,
+      map: map,
+    });
+  }
+  
+  window.initMap = initMap;
