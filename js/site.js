89c7d1a0d1e2cdacb847af002f79e5717ac885ba
Jplewa1 <Jplewa1@hawk.iit.edu>
2022-04-25 22:56:30 -0500

    fix zoom

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 function initMap() {
     const chicago = {lat: 41.85, lng: -87.65};
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 16,
       center: chicago,
     });
     const marker = new google.maps.Marker({
